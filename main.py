2192a147f66ee9adeed1e60305a63c9eb463867b
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-06-06 17:52:03 +0000

    Added basic $hi command

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+  
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hi'):
+    await message.channel.send('Hi to you!')
+
+
+client.run(os.environ['token'])
+
